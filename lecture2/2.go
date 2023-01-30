4c6405731af32d8e23de399087b9029498a49d72
amantaiaitolkyn <98659130+amantaiaitolkyn@users.noreply.github.com>
2023-01-30 14:36:36 +0600

    Delete lecture2 directory

--- lecture2/2.go
+++
@@ -1,8 +0,0 @@
-package main
-import(
-	"fmt"
-	"/github.com/amantaiaitolkyn/GO/lecture1"
-)
-func main(){
-	fmt.Println(lecture1.Hello())
-}
--- lecture2/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/amantaiaitolkyn/GO/lecture2.git
-
-go 1.19
